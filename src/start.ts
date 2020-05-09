81f593d32cbb2d900697dfc4095a0ef8c6f7c77f
i519695 <joao.soares02@sap.com>
2020-05-09 16:15:23 -0300

    Added all details to database connection

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,13 @@
+// Type ORM use it
+import "reflect-metadata";
 import Server from './Server';
+import Connection from './database/Connection';
+import { Logger } from '@overnightjs/logger';
+
+Connection.getInstance();
+let connection = Connection.getInstance().getConnection().then( () => {
+  Logger.Imp("System Connected to Database");
+})
 
 const server = new Server();
 const port : number = 3000;
